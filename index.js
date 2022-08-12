89e1ba7db800da18b9a4fba7cc0f67e25dbfee7f
caseyjramirez <caseyjramirez5@gmail.com>
2022-08-12 11:11:45 -0500

    commit tree 28de2b8f63efe821f3d6a0cd584591d491e5e2e1

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function superbowlWin(record) {
+    const result = record.find(y => y.result === "W")
+    console.log('myresult : ', result);
+    if(result) return result.year
+    return undefined
+}
+
